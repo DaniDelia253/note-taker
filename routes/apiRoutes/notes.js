4118cf88447c4731036989d7b32d28fca29ea688
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-02-10 20:14:15 -0600

    HTML and API endpoints added and tested

---
+++ routes/apiRoutes/notes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const fs = require("fs");
+const path = require("path");
+const generateUniqueId = require('generate-unique-id');
+const {notes} = require('../../db/db.json')
+
+require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = generateUniqueId()
+
+    if (!req.body.title || !req.body.text) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        notes.push(req.body);
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify({ notes }, null, 2)
+        );
+        res.json({
+            message: 'success',
+            data: req.body
+        });
+    }
+});
+
+module.exports  = router;
